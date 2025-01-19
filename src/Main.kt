28038a11e1ca4c63bd56ba579888d87b88fed338
MoisesDN <m.dias009@gmail.com>
2025-01-19 11:39:29 -0300

    For while exe

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,59 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-//    While
-//    val s = "Kotlin"
-//    var i = 100
-//
-//    while (i > 0) {
-//        println(i)
-//
-//        i--
-//    }
 
-//    Do While
-//    var i = 0
-//    do {
-//
-//        print("$i ")
-//
-//        i++
-//    } while (i < 10)
+    var i = 1
+    while (i <= 50) {
+        print("$i ")
+        i++
+    }
 
-    do {
-        print("Qual seu nome: ")
+    println()
+    println("For")
+    for (n in 1 .. 50) {
+        print("$n ")
+    }
+    println()
+    println("Segundo teste")
+    var j = 50
+    while (j >= 1) {
+        print("$j ")
+        j--
+    }
 
-        val value = readlnOrNull()
-    } while (value == "")
+    println()
+    println("For")
+    for (i in 50 downTo 1) {
+        print("$i ")
+    }
+
+    println()
+    println("Divisao por 5")
+    for (n in 1 .. 50) {
+        if (n % 5 == 0) {
+            continue
+        }
+        print("$n ")
+    }
+
+    println()
+    println("Soma")
+    var soma = 0;
+    for (i in 1 .. 500) {
+        soma += i
+    }
+    print("$soma ")
+
+    println()
+    ex5(5)
+
+}
+
+fun ex5(n: Int) {
+    for(i in 1..n) {
+        for (j in 1..i){
+            print("#")
+        }
+        println()
+    }
 }
